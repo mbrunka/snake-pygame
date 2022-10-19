152cecb32ed351510393f2bebe9f8d6788741bd6
mbrunka <michaelbrunka@gmail.com>
2022-10-19 11:58:34 +0200

    change snake, point to class
    Trashed most of old code. Currently tail collision not working

--- main.py
+++ main.py
@@ -1,110 +1,167 @@
-from ast import match_case
-from email.mime import image
 import pygame
-import sys
-import os
-
-
+from random import randint
 pygame.init()
+
 CLOCK = pygame.time.Clock()
-W_WIDTH, W_HEIGHT = 720, 720
-SNAKE_STEP = 60
-B_WIDTH, B_HEIGHT = 600, 600
-B_RECT = (SNAKE_STEP, SNAKE_STEP)
+SCREEN_WIDTH, SCREEN_HEIGHT = 720, 720
+SCREEN = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_WIDTH))
+pygame.display.set_caption('PySnake')
 
-WIN = pygame.display.set_mode((W_WIDTH, W_HEIGHT))
-pygame.display.set_caption("Snake")
-BOARD = pygame.Surface((W_WIDTH - SNAKE_STEP, W_HEIGHT - SNAKE_STEP))
+UNIT = 40
+STARTING_SPEED = 2
+SPEED_DELTA = 1
 
-FPS = 60
+class Game():
+    def __init__(self):
+        self.speed = STARTING_SPEED
+        self.wall = True
+        
+    def increase_speed(self):
+        self.speed += SPEED_DELTA
+    
 
-snake_head = pygame.image.load("assets/snake_head.png").convert_alpha()
-snake_body = pygame.image.load("assets/snake_segment.png").convert_alpha()
-
-
-def drawBoard():
-    BOARD.fill((0, 0, 0))
-    for i in range(0, B_HEIGHT, SNAKE_STEP):
-        for j in range(0, B_WIDTH, SNAKE_STEP):
-            pygame.draw.rect(BOARD, (80, 80, 80), ((i, j), B_RECT), 1)
-
-
-def rotate_head(snake_head, direction, snake_rot):
-    match direction, snake_rot:
-        case "up":
-            match snake_rot:
-                case "right":
-                    snake_head = pygame.transform.rotate(snake_head, 270)
-                case "down":
-                    snake_head = pygame.transform.rotate(snake_head, 90)
-                case "left":
-                    snake_head = pygame.transform.rotate(snake_head, 180)
-                case other:
-                    pass
-        case other:
-            pass
-    return snake_head
-
-
-def round_xy(snake_x, snake_y):
-    snake_y = int(SNAKE_STEP * round(snake_y / float(SNAKE_STEP)))
-    snake_x = int(SNAKE_STEP * round(snake_x / float(SNAKE_STEP)))
-    return (snake_x, snake_y)
+class Snake(pygame.sprite.Sprite):
+    
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface([UNIT, UNIT])
+        self.image.fill((0,255,0))
+        self.rect = self.image.get_rect()
+        
+        self.x = 2
+        self.y = 0
+        
+        self.prev_x = 0
+        self.prev_y = 0
+        
+        self.direction = (1, 0)
+        self.state = "moveright"
+        
+        self.body = [Tail(1,0),Tail(0,0)]
+        
+    def draw(self):
+        SCREEN.blit(self.image, (self.x*UNIT, self.y*UNIT))
+        for tail in self.body:
+            tail.draw()
+            
+    def change_dirct(self, dirct):
+        if dirct == "up" and self.state != "movedown":
+            self.direction = (0, -1)
+            self.state = "moveup"
+        elif dirct == "down" and self.state != "moveup":
+            self.direction = (0, 1)
+            self.state = "movedown"
+        elif dirct == "left" and self.state != "moveright":
+            self.direction = (-1, 0)
+            self.state = "moveleft"
+        elif dirct == "right" and self.state != "moveleft":
+            self.direction = (1, 0)
+            self.state = "moveright"
+                
+    def move(self):
+        self.prev_x = self.x
+        self.prev_y = self.y
+        
+        self.x += self.direction[0]
+        self.y += self.direction[1]
+        
+        prev = (self.prev_x, self.prev_y)
+        for tail in self.body:
+            prev = tail.move(prev)
+            
+    def ate(self):
+        self.body.append(Tail(self.body[-1].prev_x, self.body[-1].prev_x,))
+            
+        
+class Tail(pygame.sprite.Sprite):
+    
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface([UNIT, UNIT])
+        self.image.fill((0,0,255))
+        self.rect = self.image.get_rect()
+        
+        self.x = x
+        self.y = y
+        
+        self.prev_x = 0
+        self.prev_y = 0
+    
+    def draw(self):
+        SCREEN.blit(self.image, (self.x*UNIT, self.y*UNIT))
+        
+    def move(self, where):
+        self.prev_x = self.x
+        self.prev_y = self.y
+        
+        self.x, self.y = where
+        return (self.prev_x,self.prev_y)
+        
+    
+class Point(pygame.sprite.Sprite):
+    
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface([UNIT, UNIT])
+        self.image.fill((255,0,0))
+        self.rect = self.image.get_rect()
+        
+        self.x = 4
+        self.y = 5
+        
+    def draw(self):
+        SCREEN.blit(self.image, (self.x*UNIT, self.y*UNIT))
+        
+    def eaten(self):
+        self.x = randint (0, SCREEN_WIDTH/UNIT)
+        self.y = randint (0, SCREEN_HEIGHT/UNIT)
 
 
 def main():
-    snake_head = pygame.image.load("assets/snake_head.png").convert_alpha()
-    snake_body = pygame.image.load("assets/snake_segment.png").convert_alpha()
-    snake_x = 60
-    snake_y = 0
-    snake_rot = "right"
-    pase = 1
-    mode = "game"
-    mov = (0, 0)
-    body_prev = [[0, 0]]
-    snake_len = 1
-
+    game = Game()
+    snake = Snake()
+    point = Point()
+    background = pygame.Surface(SCREEN.get_size())
+    background = background.convert()
+    background.fill((0,0,0))
     while True:
-        CLOCK.tick(FPS)
+        CLOCK.tick(game.speed)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
-            if mode == "game":
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_RIGHT:
-                        mov = (0, 1)
-                        # if snake_rot in ["up", "down"]:
-                        # snake_x, snake_y = round_xy(snake_x, snake_y)
-                        # snake_head = rotate_head(snake_head, "up", snake_rot)
-                        snake_rot = "right"
-                    elif event.key == pygame.K_LEFT:
-                        mov = (0, -1)
-                        snake_rot = "left"
-                    elif event.key == pygame.K_UP:
-                        mov = (-1, 0)
-                        snake_rot = "up"
-                    elif event.key == pygame.K_DOWN:
-                        mov = (1, 0)
-                        snake_rot = "down"
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    snake.change_dirct("right")
+                elif event.key == pygame.K_LEFT:
+                    snake.change_dirct("left")
+                elif event.key == pygame.K_UP:
+                    snake.change_dirct("up")
+                elif event.key == pygame.K_DOWN:
+                    snake.change_dirct("down")
 
-        if mode == "game":
+        SCREEN.blit(background, (0, 0))
+        snake.move()
+        
+        #wall collide
+        if game.wall:
+            if snake.x < 0 or snake.y < 0:
+                pygame.quit()
+                exit()
+          
+        #tail collide - not working  
+        if pygame.Rect.collidelist(snake.rect, snake.body):
+            pass
+        
+        #fruit cllide
+        if snake.x == point.x and snake.y == point.y:
+            snake.ate()
+            point.eaten()
+            
+        snake.draw()
+        point.draw()
 
-            # body_prev.append([snake_x, snake_y])
-            if len(body_prev) > snake_len:
-                body_prev.pop(0)
-            snake_x = snake_x + (pase * mov[1])
-            snake_y = snake_y + (pase * mov[0])
-            for ele in body_prev:
-                ele[0] += pase * mov[1]
-                ele[1] += pase * mov[0]
-
-            drawBoard()
-            for el in body_prev:
-                BOARD.blit(snake_body, el)
-            BOARD.blit(snake_head, (snake_x, snake_y))
-            WIN.blit(BOARD, (SNAKE_STEP, SNAKE_STEP))
 
         pygame.display.update()
 
